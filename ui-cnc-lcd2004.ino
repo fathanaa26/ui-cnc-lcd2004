53764efc6be7271a15f1d7643aafd25f320fee6d
Fathan Akbar A <fathan.aa.01@gmail.com>
2023-04-15 14:17:01 +0700

    masih pusing
    masih pusing

--- ui-cnc-lcd2004.ino
+++ ui-cnc-lcd2004.ino
@@ -27,16 +27,17 @@ int nowDt;
 int menuCounter;
 bool menuChanged = false;
 bool updateSelected = false;
+bool valChanged = false;
 int rotaryButtonVal;
 
-int y_maxspeed = 5000;
-int y_speed;
+int y_maxspeed ;
+int y_speed ;
 
-int z_maxspeed = 12000;
+int z_maxspeed;
 int z_speed;
 
-int x_maxspeed = 12000;
-int x_speed;
+int x_maxspeed ;
+int x_speed ;
 
 bool bnfMode = false;
 bool snkMode = false;
@@ -81,12 +82,15 @@ void loop() {
     selectedMode();
   }
 
-  if (bnfMode) {
-    stepper_y.setSpeed(5000);
+  if(valChanged == true){
+    val_speed_change();
+  }
+
+  if (bnfMode == true) {
+
     stepper_y.moveTo(a);
     stepper_y.runSpeedToPosition();
 
-    stepper_x.setSpeed(12000);
     stepper_x.moveTo(a);
     stepper_x.runSpeedToPosition();
 
@@ -97,12 +101,11 @@ void loop() {
     }
   }
 
-  if (snkMode) {
-    stepper_y.setSpeed(1000);
+  if (snkMode == true) {
+
     stepper_y.moveTo(a);
     stepper_y.runSpeedToPosition();
 
-    stepper_x.setSpeed(1000);
     stepper_x.moveTo(a);
     stepper_x.runSpeedToPosition();
 
@@ -112,6 +115,31 @@ void loop() {
       a = -10000;
     }
   }
+}
+
+void val_speed_change(){
+  switch (menuCounter) {
+    case 3:
+      stepper_x.setMaxSpeed(x_maxspeed);
+      stepper_x.setSpeed(x_speed);
+      lcd.setCursor(4, 3);
+      lcd.print(x_maxspeed);
+      break;
+
+    case 4:
+      stepper_y.setMaxSpeed(y_maxspeed);
+      stepper_y.setSpeed(y_speed);
+      lcd.setCursor(4, 3);
+      lcd.print(y_maxspeed);
+      break;
+
+    case 5:
+      stepper_z.setMaxSpeed(z_maxspeed);
+      lcd.setCursor(4, 3);
+      lcd.print(z_maxspeed);
+      break;
+  }
+  valChanged = false;
 }
 
 void lcd_menu_update() {
@@ -129,6 +157,22 @@ void lcd_menu_update() {
       lcd.setCursor(0, 1);
       lcd.print("Mode");
       break;
+
+    case 3:
+      lcd.setCursor(0, 0);
+      lcd.print("SET SPEED X");
+      break;
+
+    case 4:
+      lcd.setCursor(0, 0);
+      lcd.print("SET SPEED Y");
+      break;
+
+    case 5:
+      lcd.setCursor(0, 0);
+      lcd.print("SET SPEED Z");
+      break;
+
   }
 
   menuChanged = false;
@@ -143,27 +187,66 @@ void selectedMode() {
     lcd.setCursor(0, 3);
     lcd.print("SELECTED");
 
+  } else if (y_maxspeed_selected == true) {
+    lcd.setCursor(0, 3);
+    lcd.print(">");
+
+  } else if (x_maxspeed_selected == true) {
+    lcd.setCursor(0, 3);
+    lcd.print(">");
+
+  } else if (z_maxspeed_selected == true) {
+    lcd.setCursor(0, 3);
+    lcd.print(">");
+
   } else {
     lcd.setCursor(0, 3);
     lcd.print("          ");
+    
   }
 
   updateSelected = false;
 }
 
 void rotary_f() {
-
   if (snkMode == true) {
 
-  } else if (bnfMode == true) {
-
-  } else {
     nowClk = digitalRead(clk_pin);
 
     if (nowClk != lastClk && nowClk == 1) {
 
       if (digitalRead(dt_pin) != nowClk) {
-        if (menuCounter < 1) {
+        if (menuCounter < 5) {
+          menuCounter++;
+        } else {
+          menuCounter = 0;
+        }
+        menuChanged = true;
+        lcd.setCursor(0,0);
+        lcd.print("            ");
+
+      } else {
+        if (menuCounter > 0) {
+          menuCounter--;
+        } else {
+          menuCounter = 5;
+        }
+        menuChanged = true;
+        lcd.setCursor(0,0);
+        lcd.print("            ");
+      }
+    }
+
+    lastClk = nowClk;
+
+  } else if (bnfMode == true) {
+
+    nowClk = digitalRead(clk_pin);
+
+    if (nowClk != lastClk && nowClk == 1) {
+
+      if (digitalRead(dt_pin) != nowClk) {
+        if (menuCounter < 5) {
           menuCounter++;
         } else {
           menuCounter = 0;
@@ -174,7 +257,86 @@ void rotary_f() {
         if (menuCounter > 0) {
           menuCounter--;
         } else {
-          menuCounter = 1;
+          menuCounter = 5;
+        }
+        menuChanged = true;
+      }
+    }
+
+    lastClk = nowClk;
+
+  } else if (x_maxspeed_selected == true) {
+    nowClk = digitalRead(clk_pin);
+    if (nowClk != lastClk && nowClk == 1) {
+      if (digitalRead(dt_pin) != nowClk) {
+        x_maxspeed = x_maxspeed + 1000;
+        x_speed = x_maxspeed;
+      } else {
+        if (x_maxspeed < 1000) {
+          x_maxspeed = 1000;
+        } else {
+          x_maxspeed = x_maxspeed - 1000;
+          x_speed = x_maxspeed;
+        }
+      }
+      valChanged = true;
+    }
+    lastClk = nowClk;
+
+  } else if (y_maxspeed_selected == true) {
+    nowClk = digitalRead(clk_pin);
+    if (nowClk != lastClk && nowClk == 1) {
+      if (digitalRead(dt_pin) != nowClk) {
+        y_maxspeed = y_maxspeed + 1000;
+        y_speed = y_maxspeed;
+      } else {
+        if (y_maxspeed < 1000) {
+          y_maxspeed = 1000;
+        } else {
+          y_maxspeed = y_maxspeed - 1000;
+          y_speed = y_maxspeed;
+        }
+      }
+      valChanged = true;
+    }
+    lastClk = nowClk;
+
+  } else if (z_maxspeed_selected == true) {
+    nowClk = digitalRead(clk_pin);
+    if (nowClk != lastClk && nowClk == 1) {
+      if (digitalRead(dt_pin) != nowClk) {
+        z_maxspeed = z_maxspeed + 1000;
+        z_speed = z_maxspeed;
+      } else {
+        if (z_maxspeed < 1000) {
+          z_maxspeed = 1000;
+        } else {
+          z_maxspeed = z_maxspeed - 1000;
+          z_speed = z_maxspeed;
+        }
+      }
+      valChanged = true;
+    }
+    lastClk = nowClk;
+
+  } else {
+    nowClk = digitalRead(clk_pin);
+
+    if (nowClk != lastClk && nowClk == 1) {
+
+      if (digitalRead(dt_pin) != nowClk) {
+        if (menuCounter < 5) {
+          menuCounter++;
+        } else {
+          menuCounter = 0;
+        }
+        menuChanged = true;
+
+      } else {
+        if (menuCounter > 0) {
+          menuCounter--;
+        } else {
+          menuCounter = 5;
         }
         menuChanged = true;
       }
@@ -189,14 +351,26 @@ void btn_f() {
 
   switch (menuCounter) {
     case 0:
-      bnfMode = true;
+      bnfMode = !bnfMode;
       updateSelected = true;
       break;
 
     case 1:
-      snkMode = true;
+      snkMode = !snkMode;
       updateSelected = true;
       break;
+    
+    case 3:
+      x_maxspeed_selected = !x_maxspeed_selected;
+      updateSelected = true;
+
+    case 4:
+      y_maxspeed_selected = !y_maxspeed_selected;
+      updateSelected = true;
+
+    case 5:
+      z_maxspeed_selected = !z_maxspeed_selected;
+      updateSelected = true;            
   }
 
   menuChanged = true;
